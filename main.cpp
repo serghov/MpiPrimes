1564bd4293d6875d611f4f24a50a1bf15b342b03
serghov <sergey.hovakimyan@gmail.com>
2016-05-09 14:13:43 +0400

    added comments

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@
 #include <vector>
 
 using namespace std;
-
+/*
+   checks wheather or not a number is prime with in O(n) time
+ */
 bool isPrime(int num)
 {	
 	if (num%2==0)
@@ -19,82 +21,86 @@ bool isPrime(int num)
 }
 
 int n;
-int world_size;
+int world_size; //number of processes involved in our calculations
 
-int *curPrimes;
+int *curPrimes;//the primes that we will calculate in THIS process
 int k;
 
-int *recvPrimes;
+int *recvPrimes;//temperary variable for primes the main process will recieve from secondary ones
 
 vector <int> allPrimes;
 
 int main(int argc, char** argv) {
 
 	int i;
-
+	//check if we have the right number of arguments
 	if (argc!=2)
 	{
 		cout <<"invalid number of arguments"<<endl;
 		return 0;
 	}
-
+	//calculate n as per requrenment
 	n = 100000 * atoi(argv[1]);
-
+	//we cant have more than n/2 primes becuase even numbers for sure cant be prime
 	curPrimes = new int[n/2];
 
+	//init mpi
+	MPI_Init(NULL, NULL);
 
-    MPI_Init(NULL, NULL);
- 
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    
-
-
-    for (i = n*world_rank / world_size;i < n*(world_rank + 1) / world_size; i++)
-    	if (isPrime(i))
-    		curPrimes[k++]=i;
-
-    if (world_rank!=0)
-    {
-    	MPI_Send(&k, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    	MPI_Send(curPrimes, k, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    }
-    //
-
-    int recvSize;
+	//find out how many processes are there
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	//find out which process are we in
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
 
+	//calculate prime numbers each process will calculate its own portion
+	for (i = n*world_rank / world_size;i < n*(world_rank + 1) / world_size; i++)
+		if (isPrime(i))
+			curPrimes[k++]=i;
+	//if we are not in the main process(process with rank 0) send our prime numbers to main process
+	if (world_rank!=0)
+	{
+		MPI_Send(&k, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(curPrimes, k, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
 
-    if (world_rank==0)
-    {
-    	for (i=0;i<k;i++)    	
-    		allPrimes.push_back(curPrimes[i]);
-    	
-    	delete curPrimes;
 
-    	for (i=1;i<world_size;i++)
-    	{
-    		MPI_Recv(&recvSize, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    		cout <<"Recieved "<<recvSize<<" primes\nretrieving"<<endl;
-    		recvPrimes = new int[recvSize];
-    		MPI_Recv(recvPrimes, recvSize, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	int recvSize;
+
+
+
+	if (world_rank==0)
+	{
+		//this runs only on main process
+		//add the primes calculated here to our large prime vector
+		for (i=0;i<k;i++)    	
+			allPrimes.push_back(curPrimes[i]);
+		//clear the memory of our primes
+		delete curPrimes;
+		//go through all the recieved primes and add them to our vector
+		for (i=1;i<world_size;i++)
+		{
+			MPI_Recv(&recvSize, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			cout <<"Recieved "<<recvSize<<" primes\nretrieving"<<endl;
+			recvPrimes = new int[recvSize];
+			MPI_Recv(recvPrimes, recvSize, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			for (int j=0;j<recvSize;j++)    	
-    			allPrimes.push_back(recvPrimes[j]);
-    		delete recvPrimes;
-    	}
+				allPrimes.push_back(recvPrimes[j]);
+			delete recvPrimes;
+		}
+		
+		cout <<"overal "<<allPrimes.size()<<" primes"<<endl;
+		//output all primes to a file
+		freopen("primes.txt","w",stdout);
 
-    	cout <<"overal "<<allPrimes.size()<<" primes"<<endl;
-    	freopen("primes.txt","w",stdout);
+		for(i=0;i<allPrimes.size();i++)
+			printf("%d ", allPrimes[i]);
+		printf("\n");
+	}
 
-    	for(i=0;i<allPrimes.size();i++)
-	    	printf("%d ", allPrimes[i]);
-    	printf("\n");
-    }
 
-    
 
-    MPI_Finalize();
-    return 0;
+	MPI_Finalize();
+	return 0;
 }
